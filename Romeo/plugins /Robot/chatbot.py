07c9ffbda5e5465e6f976039d93bae0b20321226
Romeo RJ <124047324+RRomeo-RJ@users.noreply.github.com>
2023-12-05 21:21:36 +0530

    😎

--- Romeo/plugins /Robot/chatbot.py
+++ Romeo/plugins /Robot/chatbot.py
@@ -252,4 +252,5 @@ async def Romeoprivatesticker(client: Client, message: Message):
                await message.reply_text(f"{hey}")
            if not Yo == "text":
                await message.reply_sticker(f"{hey}")
-               
+
+'''
